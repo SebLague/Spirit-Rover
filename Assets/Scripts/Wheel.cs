e1ec5e4ab11d4bad40cfcb2add22e52a83902f4a
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-11-18 15:15:10 +0200

    Dynamic wheel test; rover model

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+
+    float currHeight;
+
+    private void Start()
+    {
+        
+    }
+
+    public void Raise(float height)
+    {
+        Debug.Log("rause");
+        float deltaHeight = height - currHeight;
+        currHeight = height;
+        transform.position += transform.forward * deltaHeight;
+    }
+
+}
