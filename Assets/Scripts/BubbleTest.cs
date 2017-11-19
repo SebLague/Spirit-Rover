ac9d71e262eeb966843ab9b5a071e62d88b34627
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-11-19 15:41:29 +0200

    bubble test

---
+++ Assets/Scripts/BubbleTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTest : MonoBehaviour {
+
+	public Sprite[] s;
+	public float t;
+	SpriteRenderer r;
+	int i;
+	float nextT;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextT) {
+			nextT =Time.time + t;
+			r.sprite = s [i];
+			i++;
+			i %= s.Length;
+		}
+	}
+}
